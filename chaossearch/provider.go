e60d6e9b3ad28954c5dc0dfd3a39f343237ed416
ItamarB <itamar.ben-zaken@klarna.com>
2020-11-13 14:50:58 +0100

    receive credentials in provider config

--- chaossearch/provider.go
+++ chaossearch/provider.go
@@ -1,15 +1,62 @@
 package chaossearch
 
 import (
+	"context"
+	"terraform-provider-chaossearch/chaossearch/client"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 // Provider -
 func Provider() *schema.Provider {
 	return &schema.Provider{
-		ResourcesMap:   map[string]*schema.Resource{},
+		Schema: map[string]*schema.Schema{
+			"url": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("CHAOSSEARCH_URL", ""),
+			},
+			"access_key_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("CHAOSSEARCH_ACCESS_KEY_ID", ""),
+			},
+			"secret_access_key": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("CHAOSSEARCH_SECRET_ACCESS_KEY", ""),
+			},
+		},
+		ResourcesMap: map[string]*schema.Resource{},
 		DataSourcesMap: map[string]*schema.Resource{
 			"chaossearch_object_groups": dataSourceObjectGroups(),
 		},
+		ConfigureContextFunc: providerConfigure,
 	}
+}
+
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	url := d.Get("url").(string)
+	accessKeyID := d.Get("access_key_id").(string)
+	secretAccessKey := d.Get("secret_access_key").(string)
+
+	if url == "" {
+		return nil, diag.Errorf("Expected 'url' to be defined in provider configuration, but it was not")
+	}
+	if accessKeyID == "" {
+		return nil, diag.Errorf("Expected 'access_key_id' to be defined in provider configuration, but it was not")
+	}
+	if secretAccessKey == "" {
+		return nil, diag.Errorf("Expected 'secret_access_key' to be defined in provider configuration, but it was not")
+	}
+
+	config := client.NewConfiguration()
+	config.URL = url
+	config.AccessKeyID = accessKeyID
+	config.SecretAccessKey = secretAccessKey
+
+	client := client.NewClient(config)
+
+	return client, nil
 }
