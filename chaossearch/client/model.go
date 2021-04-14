6dfc6486e56c1206ea879279e8e795e77e7a420d
Kirill Zonov <kirill.zonov@klarna.com>
2021-04-14 23:04:34 +0200

    LX-899 Made readBucketMetadataResponse to be private

--- chaossearch/client/model.go
+++ chaossearch/client/model.go
@@ -58,17 +58,6 @@ type ReadIndexingStateRequest struct {
 	ObjectGroupName string
 }
 
-type ReadBucketMetadataResponse struct {
-	Metadata map[string]BucketMetadata `json:"Metadata"`
-}
-
-// There are other nested structures that we don't marshal here because we don't need to.
-//  Feel free to add them in the future when needed
-type BucketMetadata struct {
-	Bucket string `json:"Bucket"`
-	State  string `json:"State"`
-}
-
 type IndexingState struct {
 	ObjectGroupName string
 	Active          bool
--- chaossearch/client/operation_read_indexing_state.go
+++ chaossearch/client/operation_read_indexing_state.go
@@ -7,6 +7,18 @@ import (
 	"net/http"
 )
 
+
+type readBucketMetadataResponse struct {
+	Metadata map[string]bucketMetadata `json:"Metadata"`
+}
+
+// There are other nested structures that we don't marshal here because we don't need to.
+//  Feel free to add them in the future when needed
+type bucketMetadata struct {
+	Bucket string `json:"Bucket"`
+	State  string `json:"State"`
+}
+
 // For documentation see: https://docs.chaossearch.io/reference#bucketmodel
 func (client *Client) ReadIndexingState(ctx context.Context, req *ReadIndexingStateRequest) (*IndexingState, error) {
 	method := "GET"
@@ -16,7 +28,7 @@ func (client *Client) ReadIndexingState(ctx context.Context, req *ReadIndexingSt
 		return nil, fmt.Errorf("request failed: %s", err)
 	}
 
-	var response ReadBucketMetadataResponse
+	var response readBucketMetadataResponse
 	err = client.unmarshalJSONBody(responseBody, response)
 	if err != nil {
 		return nil, fmt.Errorf("failed to unmarshal response: %s", err)
