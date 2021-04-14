55a8a39421a73769ffa56680d0056d514f218e24
Kirill Zonov <kirill.zonov@klarna.com>
2021-04-14 16:31:01 +0200

    LX-899 Fixed example.tf to work

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ HOSTNAME=registry.terraform.io
 NAMESPACE=benzaita
 NAME=chaossearch
 BINARY=terraform-provider-${NAME}
-VERSION=0.5.3
-OS_ARCH=linux_amd64
+VERSION=0.6.0
+OS_ARCH=darwin_amd64
 
 default: install
 
--- examples/main.tf
+++ examples/main.tf
@@ -1,18 +1,23 @@
 terraform {
   required_providers {
     chaossearch = {
-      versions = ["0.2"]
+      versions = ["0.6.0"]
       source = "benzaita/chaossearch"
     }
   }
 }
 
-provider "chaossearch" {}
+provider "chaossearch" {
+    url               = "http://example.com"
+    access_key_id     = "chaossearch_access_key_id"
+    secret_access_key = "chaossearch_secret_access_key"
+    region            = "eu-west-1"
+}
 
 resource "chaossearch_object_group" "my-object-group" {
   name = "my-object-group"
   source_bucket = "<s3 bucket name>"
-  live_events_sqs_arn ="<sqs arn>"
+  live_events_sqs_arn ="arn:aws:sqs:sqs_sqs"
 
   filter_json = jsonencode({
     AND = [
